d6275c1daedbfe991eef212031649176958bdec6
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-06-10 13:41:27 +0545

    Add the region of interest

--- lane.py
+++ lane.py
@@ -1,8 +1,12 @@
 import cv2 as cv
 import numpy as np
+# import matplotlib.pyplot as plt
 
 img = cv.imread('road.png')
-# gray = cv2.cvtColor()
-cv.imshow('road.png',img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+# plt.imshow(gray)
+# plt.show()
+cv.imshow('Image', gray)
 cv.waitKey(0)
 cv.destroyAllWindows()
